388af07ae4b834088014bceaea5a931f9c688f08
Audrey Alcantara <adrianaquin22@gmail.com>
2022-11-19 12:40:07 -0500

    added moment.js and displayed current date on header of page

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// in the html. **document.ready()?
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
+  // local storage. **this is saying that if the id of time block 4pm for ex is '4o' than '4o' should be the key to retrieve the event
+  // HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
@@ -20,4 +21,9 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  //var dateEl = $('<h3>')
+  var dateEl = $('#currentDay')
+  var todayDate = moment().format('dddd')
+  dateEl.text(`Today is: ${todayDate}`)
+  
 });
